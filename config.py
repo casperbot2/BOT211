58e2bffccd5b0fcbf78475e99fc3db9e762ba689
casperbot2 <aliahmd2233445t@gmail.com>
2019-11-15 16:52:06 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# Basic Config (For all bots)
+userToken = [''] # User Token here, multiple accounts may work. example 'token','token'
+DiscordChannel = '643759239789019137' # Channel ID,
+
+#Picture spam
+DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
---
+++ start.py
@@ -0,0 +1,42 @@
+import sys
+import subprocess
+import os
+from time import sleep
+from config import *
+
+w1 = "EDIT YOUR CONFIG.PY BEFORE USING!\n"
+
+def worker(file,token):
+    print(token + " " + file)
+    subprocess.call(file, shell=True)
+
+for char in w1:
+    sleep(0.01)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+sleep(0.5)
+print("Type one of the following numbers to launch that spammmer")
+print("1 : Image Spammer - Spam random images in a selected folder")
+print("2 : Insult Spammer - Picks insults offline and spams them")
+print("3 : Text Spammer - Write your own text to spam")
+
+in_pick = float(input("3: "))
+
+if in_pick == 1:
+    for token in userToken:
+        p = subprocess.Popen(['python', 'discord_image_spam.py', token],shell=True)
+            
+if in_pick == 2:
+    for token in userToken:
+        p = subprocess.Popen(['python','discord_insult_spam.py', token],shell=True)
+        
+if in_pick == 3:
+    spam_text = input("يا عباس يا عباس يا عباس : ")
+    print('python discord_text_spam.py ' + "'"+spam_text+"'")
+    for token in userToken:
+        print('Loading Token')
+        p = subprocess.Popen(['python','discord_text_spam.py',token,spam_text],shell=True)
+        sleep(1)
+        print("Token Loaded.")
+        print(' ')
+p.wait()
